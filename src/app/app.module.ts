30d8b9e32afeff7f75ebb69a852d3d5a9ec951e8
Agung Januardi <agungjanuardi16@gmail.com>
2023-12-19 21:59:36 +0700

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { AppRoutingModule } from './app.routing';
+import { NavbarModule } from './shared/navbar/navbar.module';
+import { SidebarModule } from './sidebar/sidebar.module';
+
+import { AppComponent } from './app.component';
+
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+
+@NgModule({
+  imports: [
+    BrowserAnimationsModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule,
+    NavbarModule,
+    SidebarModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    AdminLayoutComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
